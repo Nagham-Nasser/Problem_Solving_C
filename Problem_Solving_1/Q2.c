6a4fa4e9853602d0b5010ca6c4f48208a4f62b0e
Nagham Nasser <144176670+Nagham-Nasser@users.noreply.github.com>
2023-09-10 20:14:46 +0300

    Update Q2.c

--- Problem_Solving_1/Q2.c
+++ Problem_Solving_1/Q2.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 
 int main(){
-	/* (Calculating the Product of Odd Integers) 
-	Write a program that calculates and prints the 
-	product of the odd integers from 1 to 15 */ 
+/* Write a program that calculates and prints the product of the odd integers from 1 to 15 */ 
+	
 	unsigned char x;
 	unsigned int prod = 1;
 	for(x = 1; x <= 15; x++){
