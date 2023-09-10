f241424af54be54ae1ee2aa45e3522679022a1de
Nagham Nasser <144176670+Nagham-Nasser@users.noreply.github.com>
2023-09-10 21:27:54 +0300

    Update Q6.c

--- Problem_Solving_3/Q6.c
+++ Problem_Solving_3/Q6.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/* There is a function that returns:
+• 1 if x is positive.
+• -1 if x is negative.
+• 0 if x is equal to 0.
+You are given an integer array nums. Let product be the product of all values in the array nums. Find the sign of the product 
+*/
+
 int arraySign(char* nums, int numsSize);
 
 
