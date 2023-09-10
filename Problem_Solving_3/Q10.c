169885ef3dd2dd7f2dd33d614b6949ac87c78452
Nagham Nasser <144176670+Nagham-Nasser@users.noreply.github.com>
2023-09-10 21:30:29 +0300

    Update Q10.c

--- Problem_Solving_3/Q10.c
+++ Problem_Solving_3/Q10.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/* Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last 
+integer of the previous row. */
+
 typedef enum{
 	false = 0,
 	true
