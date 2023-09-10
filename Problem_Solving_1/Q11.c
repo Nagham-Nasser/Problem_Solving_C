6715db96970a430222e9868cb4c53ea990372e58
Nagham Nasser <144176670+Nagham-Nasser@users.noreply.github.com>
2023-09-10 20:12:59 +0300

    Update Q11.c

--- Problem_Solving_1/Q11.c
+++ Problem_Solving_1/Q11.c
@@ -3,6 +3,13 @@
 short qualityPoints (short avg );
 
 int main(){
+/* Write a function qualityPoints that inputs a student’s average and returns :
+	4 if a student's average is 90–100, 
+	3 if the average is 80–89,
+	2 if the average is 70–79, 
+	1 if the average is 60–69, 
+	0 if the average is lower than 60.
+*/
 	short x;
 	printf("Enter Your Average : ");
 	scanf("%d", &x);
